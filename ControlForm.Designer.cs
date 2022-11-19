a7dde332737995b7d68a3d97620661c5265b7cb7
eugen <eugen@ROSEA>
2022-11-19 18:06:37 +1030

    Add project files.

---
+++ ControlForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace HarmoniaRemote
+{
+    partial class ControlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDeflate = new System.Windows.Forms.Button();
+            this.btnInflate = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb.Location = new System.Drawing.Point(0, 200);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(800, 250);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnDeflate
+            // 
+            this.btnDeflate.Location = new System.Drawing.Point(211, 142);
+            this.btnDeflate.Name = "btnDeflate";
+            this.btnDeflate.Size = new System.Drawing.Size(97, 33);
+            this.btnDeflate.TabIndex = 3;
+            this.btnDeflate.Text = "Deflate";
+            this.btnDeflate.UseVisualStyleBackColor = true;
+            this.btnDeflate.Click += new System.EventHandler(this.btnDeflate_Click);
+            // 
+            // btnInflate
+            // 
+            this.btnInflate.Location = new System.Drawing.Point(108, 142);
+            this.btnInflate.Name = "btnInflate";
+            this.btnInflate.Size = new System.Drawing.Size(97, 33);
+            this.btnInflate.TabIndex = 4;
+            this.btnInflate.Text = "Inflate";
+            this.btnInflate.UseVisualStyleBackColor = true;
+            this.btnInflate.Click += new System.EventHandler(this.btnInflate_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(348, 142);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(97, 33);
+            this.btnForward.TabIndex = 5;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(451, 142);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(97, 33);
+            this.btnReverse.TabIndex = 6;
+            this.btnReverse.Text = "Reverse";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // ControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnInflate);
+            this.Controls.Add(this.btnDeflate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtb);
+            this.Name = "ControlForm";
+            this.Padding = new System.Windows.Forms.Padding(0, 200, 0, 0);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ControlForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDeflate;
+        private System.Windows.Forms.Button btnInflate;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnReverse;
+    }
+}
+
---
+++ ControlForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace HarmoniaRemote
+{
+    public partial class ControlForm : Form
+    {
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+
+        private SerialPort sp = new SerialPort("COM4", 9600);
+        
+        private void ControlForm_Load(object sender, EventArgs e)
+        {
+            sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+            try
+            {
+                sp.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                
+                string txt = sp.ReadLine();
+                if (!txt.StartsWith("VMDPE"))
+                {
+                    Console.WriteLine(txt);
+                    SetControlText(rtb, txt);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public static void SetControlText(Control control, string text)
+        {
+            control.Invoke((MethodInvoker)delegate { control.Text = control.Text +  text; });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine(this.textBox1.Text);
+        }
+
+        private void btnDeflate_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine("DEFLATE");
+        }
+
+        private void btnInflate_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine("INFLATE");
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine("FORWARD");
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            sp.WriteLine("REVERSE");
+        }
+    }
+}
